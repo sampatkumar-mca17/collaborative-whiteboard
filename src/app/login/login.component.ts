6c6defa67e1fbfa7d34df652b6e250407e89a25b
Varniha <varni3121@gmail.com>
2024-05-03 15:57:47 +0530

    implemented input field error (mat-error)

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -13,6 +13,9 @@ import { MatCheckboxModule } from '@angular/material/checkbox';
 import { faEnvelope, faLock, faCheckDouble, faUser } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { ToastrService } from 'ngx-toastr';
+import {takeUntilDestroyed} from '@angular/core/rxjs-interop';
+import {FormControl, Validators,ReactiveFormsModule} from '@angular/forms'
+import {merge} from 'rxjs';
 
 @Component({
   selector: 'app-login',
@@ -29,52 +32,119 @@ import { ToastrService } from 'ngx-toastr';
     MatTabsModule,
     MatSnackBarModule,
     FontAwesomeModule,
+    ReactiveFormsModule,
   ],
   styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
-  email = '';
-  password = '';
-  confirmpassword = '';
-  errorMsg_user = '';
-  errorMsg_pwd = '';
-  errorMsg_ruser='';
-  errorMsg_rpwd='';
-  errorMsg_confirmPwd = '';
   checked: false;
-  remail = '';
-  rpassword = '';
   faEnvelope = faEnvelope;
   faLock = faLock;
   faCheckDouble = faCheckDouble;
   faUser = faUser;
-  toaster = inject(ToastrService)
 
-  constructor(private router: Router, private sharedService: SharedService) {
+  email = new FormControl('', [Validators.required, Validators.email]);
+  password = new FormControl('', [Validators.required]);
+  remail = new FormControl('', [Validators.required]);
+  rpassword = new FormControl('', [Validators.required]);
+  confirmpassword = new FormControl('', [Validators.required])
+  errorMsg_email = '';
+  errorMsg_pwd = '';
+  errorMsg_reg_email = '';
+  errorMsg_reg_pwd='';
+  errorMsg_confirm_pwd='';
 
+  constructor(private router: Router, private toaster: ToastrService, private sharedService: SharedService) 
+  {
+    // To display error messages on each field
+    merge(
+      this.email.statusChanges, 
+      this.email.valueChanges,
+      this.password.statusChanges,
+      this.password.valueChanges,
+      this.remail.statusChanges,
+      this.remail.valueChanges,
+      this.rpassword.statusChanges,
+      this.rpassword.valueChanges,
+      this.confirmpassword.statusChanges,
+      this.confirmpassword.valueChanges
+    )
+      .pipe(takeUntilDestroyed())
+      .subscribe(() => {
+        this.updateEmailErrorMessage();
+        this.updatePasswordErrorMessage();
+        this.updateRegEmailErrorMessage();
+        this.updateRegPasswordErrorMessage();
+        this.updateConfirmPasswordErrorMessage();
+      });
   }
 
   ngOnInit(): void {}
 
-  login() {
-    // Reset error messages
-    this.errorMsg_user = '';
-    this.errorMsg_pwd = '';
+  updateEmailErrorMessage() {
+    this.errorMsg_email = this.email.hasError('required')
+      ? 'You must enter an email'
+      : this.email.hasError('email')
+      ? 'Not a valid email'
+      : '';
+  }
 
-    if (this.email.trim().length === 0) {
-      this.errorMsg_user = '*Email is required!';
-    } else if (this.password.trim().length === 0) {
-      this.errorMsg_pwd = '*Password is required!';
+  updatePasswordErrorMessage() {
+    this.errorMsg_pwd = this.password.hasError('required')
+      ? 'You must enter a password'
+      : this.password.hasError('minlength')
+      ? 'Password must be at least 8 characters long'
+      : '';
+  }
+
+  updateRegEmailErrorMessage() {
+    this.errorMsg_reg_email = this.remail.hasError('required')
+      ? 'You must enter an email'
+      : this.remail.hasError('email')
+      ? 'Not a valid email'
+      : '';
+  }
+
+  updateRegPasswordErrorMessage() {
+    this.errorMsg_reg_pwd = this.rpassword.hasError('required')
+      ? 'You must enter a password'
+      : this.rpassword.hasError('minlength')
+      ? 'Password must be at least 8 characters long'
+      : '';
+  }
+
+  updateConfirmPasswordErrorMessage() {
+    this.errorMsg_confirm_pwd = this.confirmpassword.hasError('required')
+      ? 'You must confirm your password'
+      : this.rpassword.value !== this.confirmpassword.value
+      ? 'Passwords do not match'
+      : '';
+  }
+
+  login() {
+
+    this.email.markAsTouched();
+    this.password.markAsTouched();
+
+    this.updateEmailErrorMessage();
+    this.updatePasswordErrorMessage();
+
+    if (this.email.invalid || this.password.invalid) {
+      return;
     }
+
+    // Reset error messages
+    this.errorMsg_email = '';
+    this.errorMsg_pwd = '';
 
     // Check if the user already exists in local storage
     const existingUsers = JSON.parse(localStorage.getItem('users') || '[]');
     const existingUser = existingUsers.find(
       (user: any) =>
-        user.email === this.email && user.password === this.password
+        user.email === this.email.value && user.password === this.password.value
     );
     if (existingUser) {
-      this.sharedService.setUserEmail(this.email);
+      this.sharedService.setUserEmail(this.email.value);
       this.toaster.success("Login Successful!","Success", {
         timeOut: 3000,
       })
@@ -89,49 +159,51 @@ export class LoginComponent implements OnInit {
   }
 
   register() {
+
+    this.remail.markAsTouched();
+    this.rpassword.markAsTouched();
+    this.confirmpassword.markAsTouched();
+
+    // Trigger validation
+    this.updateRegEmailErrorMessage();
+    this.updateRegPasswordErrorMessage();
+    this.updateConfirmPasswordErrorMessage();
+
+    // Check if there are any validation errors
+    if (this.remail.invalid || this.rpassword.invalid || this.confirmpassword.invalid) {
+      return;
+    }
+
     // Reset error messages
-    this.errorMsg_ruser = '';
-    this.errorMsg_rpwd = '';
-    this.errorMsg_confirmPwd = '';
-
-    // Validate username
-    if (this.remail.trim().length === 0) {
-      this.errorMsg_user = '*Email is required!';
-      return;
-    }
-
-    // Validate password
-    if (this.rpassword.trim().length === 0) {
-      this.errorMsg_rpwd = '*Password is required!';
-      return;
-    }
-
-    // Validate confirm password
-    if (this.confirmpassword.trim().length === 0) {
-      this.errorMsg_confirmPwd = '*Please confirm your password!';
-      return;
-    }
-
-    if (this.rpassword !== this.confirmpassword) {
-      this.errorMsg_confirmPwd = '*Passwords do not match!';
-      return;
-    }
+    this.errorMsg_reg_email = '';
+    this.errorMsg_reg_pwd = '';
+    this.errorMsg_confirm_pwd = '';
 
     // Check if the user already exists in local storage
     const existingUsers = JSON.parse(localStorage.getItem('users') || '[]');
     const existingUser = existingUsers.find(
-      (user: any) => user.email === this.remail
+      (user: any) => user.email === this.remail.value
     );
     if (existingUser) {
-      this.errorMsg_ruser = '*Email already exists!';
+      this.toaster.error ("Email already exists!", "Error", {
+        timeOut: 2000,
+      });
+      return;
+    }
+
+    if (this.rpassword.value !== this.confirmpassword.value) {
+      this.toaster.error ("Passwords do not match!", "Error", {
+        timeOut: 2000,
+      });
       return;
     }
 
     // If everything is valid, store the new user in local storage
-    existingUsers.push({ email: this.remail, password: this.rpassword });
+    existingUsers.push({ email: this.remail.value, password: this.rpassword.value });
     localStorage.setItem('users', JSON.stringify(existingUsers));
     this.toaster.success("Account Created Successfully! Please Login!","Success", {
       timeOut: 3000,
-    })
-    }
+    });
+}
+
 }
