93248399abdd1d2b886c466ffdc044a0e6484cc4
Varniha <varni3121@gmail.com>
2024-05-06 12:19:24 +0530

    changed error messages format

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -10,12 +10,17 @@ import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCheckboxModule } from '@angular/material/checkbox';
-import { faEnvelope, faLock, faCheckDouble, faUser } from '@fortawesome/free-solid-svg-icons';
+import {
+  faEnvelope,
+  faLock,
+  faCheckDouble,
+  faUser,
+} from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { ToastrService } from 'ngx-toastr';
-import {takeUntilDestroyed} from '@angular/core/rxjs-interop';
-import {FormControl, Validators,ReactiveFormsModule} from '@angular/forms'
-import {merge} from 'rxjs';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { FormControl, Validators, ReactiveFormsModule } from '@angular/forms';
+import { merge } from 'rxjs';
 
 @Component({
   selector: 'app-login',
@@ -47,18 +52,21 @@ export class LoginComponent implements OnInit {
   password = new FormControl('', [Validators.required]);
   remail = new FormControl('', [Validators.required]);
   rpassword = new FormControl('', [Validators.required]);
-  confirmpassword = new FormControl('', [Validators.required])
+  confirmpassword = new FormControl('', [Validators.required]);
   errorMsg_email = '';
   errorMsg_pwd = '';
   errorMsg_reg_email = '';
-  errorMsg_reg_pwd='';
-  errorMsg_confirm_pwd='';
+  errorMsg_reg_pwd = '';
+  errorMsg_confirm_pwd = '';
 
-  constructor(private router: Router, private toaster: ToastrService, private sharedService: SharedService) 
-  {
+  constructor(
+    private router: Router,
+    private toaster: ToastrService,
+    private sharedService: SharedService
+  ) {
     // To display error messages on each field
     merge(
-      this.email.statusChanges, 
+      this.email.statusChanges,
       this.email.valueChanges,
       this.password.statusChanges,
       this.password.valueChanges,
@@ -122,7 +130,6 @@ export class LoginComponent implements OnInit {
   }
 
   login() {
-
     this.email.markAsTouched();
     this.password.markAsTouched();
 
@@ -145,21 +152,18 @@ export class LoginComponent implements OnInit {
     );
     if (existingUser) {
       this.sharedService.setUserEmail(this.email.value);
-      this.toaster.success("Login Successful!","Success", {
+      this.toaster.success('Login Successful!', 'Success', {
         timeOut: 3000,
-      })
+      });
       this.router.navigate(['whiteboard']);
-    }
-
-    else {
-      this.toaster.error("*Invalid Credentials!","Error", {
+    } else {
+      this.toaster.error('*Invalid Credentials!', 'Error', {
         timeOut: 2000,
-      })
+      });
     }
   }
 
   register() {
-
     this.remail.markAsTouched();
     this.rpassword.markAsTouched();
     this.confirmpassword.markAsTouched();
@@ -170,7 +174,11 @@ export class LoginComponent implements OnInit {
     this.updateConfirmPasswordErrorMessage();
 
     // Check if there are any validation errors
-    if (this.remail.invalid || this.rpassword.invalid || this.confirmpassword.invalid) {
+    if (
+      this.remail.invalid ||
+      this.rpassword.invalid ||
+      this.confirmpassword.invalid
+    ) {
       return;
     }
 
@@ -185,25 +193,31 @@ export class LoginComponent implements OnInit {
       (user: any) => user.email === this.remail.value
     );
     if (existingUser) {
-      this.toaster.error ("Email already exists!", "Error", {
+      this.toaster.error('Email already exists!', 'Error', {
         timeOut: 2000,
       });
       return;
     }
 
     if (this.rpassword.value !== this.confirmpassword.value) {
-      this.toaster.error ("Passwords do not match!", "Error", {
+      this.toaster.error('Passwords do not match!', 'Error', {
         timeOut: 2000,
       });
       return;
     }
 
     // If everything is valid, store the new user in local storage
-    existingUsers.push({ email: this.remail.value, password: this.rpassword.value });
-    localStorage.setItem('users', JSON.stringify(existingUsers));
-    this.toaster.success("Account Created Successfully! Please Login!","Success", {
-      timeOut: 3000,
+    existingUsers.push({
+      email: this.remail.value,
+      password: this.rpassword.value,
     });
-}
-
+    localStorage.setItem('users', JSON.stringify(existingUsers));
+    this.toaster.success(
+      'Account Created Successfully! Please Login!',
+      'Success',
+      {
+        timeOut: 3000,
+      }
+    );
+  }
 }
