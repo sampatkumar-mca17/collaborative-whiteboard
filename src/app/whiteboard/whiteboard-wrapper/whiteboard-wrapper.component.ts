97b0a679bdfbbaa5f565599c7f6d0a4ed0735071
sampat <sampat.kumar@aquera.com>
2025-05-30 21:45:54 +0530

    nits

--- src/app/whiteboard/whiteboard-wrapper/whiteboard-wrapper.component.ts
+++ src/app/whiteboard/whiteboard-wrapper/whiteboard-wrapper.component.ts
@@ -77,9 +77,9 @@ export class WhiteboardWrapperComponent {
     }
     if(this.boardContainer && !this.resizedIframe){
       this.resizedIframe = true;
-      this.resizeBoardPickerIframe();
+      this.openBoardsPicker(this.boardContainer);
       setTimeout(()=>{
-        this.openBoardsPicker(this.boardContainer);
+        this.resizeBoardPickerIframe();
       },100)
     }
   }
